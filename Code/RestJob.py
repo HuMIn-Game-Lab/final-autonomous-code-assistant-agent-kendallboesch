6c200ec329586a8059b012a5ad63cd6a6506f592
kendallboesch <83316196+kendallboesch@users.noreply.github.com>
2023-12-04 22:12:28 -0600

    writing fixed errors to fixedErrors.json

--- Code/RestJob.py
+++ Code/RestJob.py
@@ -22,16 +22,12 @@ if instruction == 'errorsolve':
 
     file = open(promptFile)
     data =json.load(file)
-    resultFormatInstruct = "{\n{\'colNum\' : #}, \n {\'errorMessage\': \'\'},\n{\'file\': \'\'},\n{\'lineNum\': #},\n{\'nextLine\':\'\'},\n{\'previousLine\':\'\'},\n{\'resDescr\': \'\'},\n{\'src\': \'\'},\n{\'srcResolved\': \'\'}\n}\n"
-    basePromptInstruct=f"This is the format of an error object:\n {resultFormatInstruct}\n"
-    instructionInstruct="For each error object given, provide the resolved C++ code in the \'srcResolved\' member, and provide a description of the fix in the \'resDescr\' member.\n"
-    jsonOnlyInstruct = "Return only the updated error object, do not prompt your response.\n"
 
-    resultFormat = "{\n{\'colNum\' : #}, \n {\'errorMessage\': \'\'},\n{\'file\': \'\'},\n{\'lineNum\': #},\n{\'resDescr\': \'\'},\n{\'src\': \'\'},\n{\'srcResolved\': \'\'}\n}\n"
+    resultFormat = "{\n{\'colNum\' : #}, \n {\'errorMessage\': \'\'},\n{\'file\': \'\'},\n{\'lineNum\': #},\n{\'nextLine\':\'\'},\n{\'previousLine\':\'\'},\n{\'resDescr\': \'\'},\n{\'src\': \'\'},\n{\'srcResolved\': \'\'}\n}\n"
     basePrompt=f"This is the format of an error object:\n {resultFormat}\n"
-    instruction="For each error object given, provide the resolved C++ code in the \'srcResolved\' member, and provide a description of the fix in the \'resDescr\' member.\n"
-    details="\'srcResolved\' should only contain valid c++ code, unless the error cannot be resolved, then value should be 'need more context' \n Leave the rest of the errorObject input the same. "
-    jsonOnly = "Return only the updated error object, do not prompt your response.\n"
+    instruction="For each error object given, provide the resolved C++ code in the \'srcResolved\' member, and provide a description of the fix in the \'resDescr\' member. Leave the incorrect code in the 'src' member.\n"
+    details="\'srcResolved\' should only contain valid c++ code. \n "
+    jsonOnly = "Your response should be in json format and have an entry for each file, with a nested list of the updated error objects. Return only the updated json do not prompt your response.\n"
     file.close()
 
     #print(prompt)
@@ -40,7 +36,12 @@ if instruction == 'errorsolve':
             write = json.load(json_file)
     else: 
         write = []
-        
+    
+    if os.path.exists('fixedErrors.json'):
+        with open('fixedErrors.json') as outFile:
+            writer = json.load(outFile)
+    else:
+        writer = []
 
     for file, errors in data.items():
         # print(f"File: {file}")
@@ -65,11 +66,19 @@ if instruction == 'errorsolve':
         # print(response['choices'][0]['text'])
         text = response['choices'][0]['text']
         LLMresponse = text[len(prompt):]
+        lowerLLMres = LLMresponse.lower()
+        if lowerLLMres.find('```json\n') != -1:
+            print('had ```json\n\n')
+            LLMresponse = LLMresponse[8:]
+            LLMresponse = LLMresponse[:-3]
         print(LLMresponse)
         
+        writer.append(LLMresponse)
         write.append(response)
     with open("Data/convo.json", 'w') as json_file:
         json.dump(write, json_file, indent=4)
+    with open('fixedErrors.json', 'w') as outFile:
+        json.dump(writer, outFile, indent=4)
     
 elif instruction == 'flowgen':
     print("FLOWGEN")
