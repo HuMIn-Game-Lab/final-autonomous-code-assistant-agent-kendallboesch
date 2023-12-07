a6bcdd512f3ea6e0d672c472d9912c82fcff26b4
kendallboesch <83316196+kendallboesch@users.noreply.github.com>
2023-12-07 12:25:44 -0600

    added copy of folder in fodler so I can change back to original errors after a run

---
+++ Code/toCompile/toCompile copy/demoerror.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+/**************
+ * 
+ * MAKE TARGET: make demoerror 
+ * ERROR TYPES: 
+ *          - Undeclared identifier 
+ *          - Expected ';' after return statement 
+***************/
+
+
+int main()
+{
+    std::cout << "in demoerror.cpp" << endl;
+    return 0
+}
---
+++ Code/toCompile/toCompile copy/typeerror.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+
+int main()
+{
+
+    std::string str = "this is a string";
+    char chr = 'c'; 
+    chr = str; 
+    return 0; 
+}
